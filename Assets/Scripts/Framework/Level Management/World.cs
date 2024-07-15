d37e0d40b9fdea1f2265c00d23b4ddbb2611ef2b
Doğa Can Kılıç <dogabeey@gmail.com>
2024-07-15 19:48:06 +0300

    updates

--- Assets/Scripts/Framework/Level Management/World.cs
+++ Assets/Scripts/Framework/Level Management/World.cs
@@ -24,7 +24,7 @@ namespace Dogabeey
 
         private LevelScene currentLevel;
 
-        internal LevelScene CurrentLevel { get => currentLevel;
+        public LevelScene CurrentLevel { get => currentLevel;
             set
             {
                 currentLevel = value;
